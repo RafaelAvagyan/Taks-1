615bdfa66340c61a6a8efc255b39f207044a78e0
RafaelAvagyan <rafael.avagyan.97@mail.ru>
2025-06-16 13:05:32 +0300

    entering a value

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,77 @@
-import React, { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React, { useState } from 'react';
+import styles from './App.module.css';
 
 function App() {
-    const [count, setCount] = useState(0);
+	const [value, setValue] = useState('');
+	const [list, setList] = useState([]);
+	const [error, setError] = useState('');
 
-    return React.createElement(
-        React.Fragment,
-        null,
-        React.createElement(
-            "div",
-            null,
-            React.createElement(
-                "a",
-                { href: "https://vite.dev", target: "_blank" },
-                React.createElement("img", {
-                    src: viteLogo,
-                    className: "logo",
-                    alt: "Vite logo",
-                })
-            ),
-            React.createElement(
-                "a",
-                { href: "https://react.dev", target: "_blank" },
-                React.createElement("img", {
-                    src: reactLogo,
-                    className: "logo react",
-                    alt: "React logo",
-                })
-            )
-        ),
-        React.createElement("h1", null, "Vite + React"),
-        React.createElement(
-            "div",
-            { className: "card" },
-            React.createElement(
-                "button",
-                { onClick: () => setCount((count) => count + 1) },
-                `count is ${count}`
-            ),
-            React.createElement(
-                "p",
-                null,
-                "Edit ",
-                React.createElement("code", null, "src/App.jsx"),
-                " and save to test HMR"
-            )
-        ),
-        React.createElement(
-            "p",
-            { className: "read-the-docs" },
-            "Click on the Vite and React logos to learn more"
-        ),
-        React.createElement("div", null, new Date().getFullYear())
-    );
+	const isValueValid = value.length >= 3;
+
+	const onInputButtonClick = () => {
+		const promptValue = prompt();
+
+		if (promptValue.length >= 3) {
+			setValue(promptValue);
+			setError('');
+		} else {
+			setError('Введенное значение должно содержать минимум 3 символа');
+		}
+	};
+
+	const onAddButtonClick = () => {
+		if (value.length >= 3) {
+			const date = new Date();
+
+			const day = String(date.getDate()).padStart(2, '0');
+			const month = String(date.getMonth() + 1).padStart(2, '0');
+			const year = date.getFullYear();
+			const hours = String(date.getHours()).padStart(2, '0');
+			const minutes = String(date.getMinutes()).padStart(2, '0');
+			const seconds = String(date.getSeconds()).padStart(2, '0');
+
+			const formattedDate = `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
+			setList((prev) => [...prev, { id: Date.now(), value, formattedDate }]);
+			setValue('');
+			setError('');
+		}
+	};
+
+	return (
+		<div className={styles.app}>
+			<h1 className={styles.pageHeading}>Ввод значения</h1>
+			<p className={styles.noMarginText}>
+				Текущее значение <code>value</code>: "
+				<output className={styles.currentValue}>{value}</output>"
+			</p>
+			{error !== '' ? <div className={styles.error}>{error}</div> : ''}
+			<div className={styles.buttonsContainer}>
+				<button onClick={onInputButtonClick} className={styles.button}>
+					Ввести новое
+				</button>
+				<button
+					onClick={onAddButtonClick}
+					className={styles.button}
+					disabled={!isValueValid}
+				>
+					Добавить в список
+				</button>
+			</div>
+			<div className={styles.listContainer}>
+				<h2 className={styles.listHeading}>Список:</h2>
+				{list.length ? (
+					<ul className={styles.list}>
+						{list.map(({ value, id, formattedDate }) => (
+							<li key={id}>
+								{value} <span>{formattedDate}</span>
+							</li>
+						))}
+					</ul>
+				) : (
+					<p className={styles.noMarginText}>Нет добавленных элементов</p>
+				)}
+			</div>
+		</div>
+	);
 }
-
 export default App;
